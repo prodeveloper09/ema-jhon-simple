e73048e65670f595719df07c71e595d64fc68818
MD Imran <138973820+prodeveloper09@users.noreply.github.com>
2026-05-02 12:10:27 +0600

    product card design update

---
+++ src/component/Product/Product.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faStar } from "@fortawesome/free-solid-svg-icons";
+
+const Product = (props) => {
+  const { img, name, seller, price, star, stock } = props.product;
+
+  console.log(props.product);
+
+  return (
+    <div className="product-card">
+      <div className="product-img">
+        <img src={img} alt={name} />
+      </div>
+
+      <div className="product-info">
+        <h4 className="product-name">{name}</h4>
+
+        <p className="product-seller">
+          by <span>{seller}</span>
+        </p>
+
+        <div className="product-rating">
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <FontAwesomeIcon icon={faStar} />
+          <span> ({star})</span>
+        </div>
+
+        <p className="product-price">${price}</p>
+
+        <p className="product-stock">Only {stock} left in stock - order soon</p>
+
+        <button className="add-cart-btn">
+          <FontAwesomeIcon icon={faCartShopping} /> Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
